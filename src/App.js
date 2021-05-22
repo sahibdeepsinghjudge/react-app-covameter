f4813325230498d36206797706cdfc1ceafb100a
unknown <sahibdeepsinghj@gmail.com>
2021-05-22 18:53:12 +0530

    app download

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
        
        <div className="footer container bg-ioslight p-3 mt-3 rounded text-center font-bold-sm">
       Api by <a href="https://www.covid19india.org/">https://www.covid19india.org/</a> 
+      <br></br>
+      <a href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40sahibdeep_jwd/Cova-Meter-6a7d0a99fc694ccf9357e429b3aeff93-signed.apk"><button className="btn btn-dark">Download app</button></a>
       <p>Developed by: jwd</p>
     </div>
      
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
-     
     <App />
   </React.StrictMode>,
   document.getElementById('root')
